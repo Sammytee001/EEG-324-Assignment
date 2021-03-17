74e91141aea7aa4defecdc2df3c40cba9b0f384c
samuel osifeso <osifeso18@gmail.com>
2021-03-17 03:19:16 +0100

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEG_324_Assignment
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEG_324_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empl1 = new Employee("Samuel", "Osifeso", 150000000);
+            Employee empl2 = new Employee("Emmanuel", "Adefulu", 120000000);
+
+            Console.WriteLine("Employee 1 Firstname: {0}", empl1.FirstName);
+            Console.WriteLine("Employee 1 Lastname: {0}", empl1.LastName);
+            Console.WriteLine("Employee 1 Salary: {0}", empl1.Salary);
+
+            Console.WriteLine("Employee 2 Firstname: {0}", empl2.FirstName);
+            Console.WriteLine("Employee 2 Lastname: {0}", empl2.LastName);
+            Console.WriteLine("Employee 2 Salary: {0}", empl2.Salary);
+
+            Console.WriteLine();
+            Console.WriteLine("After 10% raise");
+
+            empl1.Salary = empl1.Salary * (decimal)1.1;
+            empl2.Salary = empl2.Salary * (decimal)1.1;
+            Console.WriteLine("Employee 1 new salary: {0}", empl1.Salary);
+            Console.WriteLine("Employee 2 new salary: {0}", empl2.Salary);
+
+            Console.ReadLine();
+        }
+    }
+}
